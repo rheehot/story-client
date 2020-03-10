391a4bd1e83e2576c592ccf61e41471431d02472
오민섭 <ominseob@ominseob-ui-MacBookPro.local>
2020-03-11 00:42:13 +0900

    [client] init

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier/@typescript-eslint'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'svelte3'],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  rules: {
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    'import/first': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/prefer-interface': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'no-use-before-define': 0,
+    'no-undef': 0,
+    'no-useless-catch': 0,
+    '@typescript-eslint/camelcase': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'svelte/parse-error': 0,
+  },
+};
